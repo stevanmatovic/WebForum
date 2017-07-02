02deea8674bc92a5a3a5e0e58a9e14ff2339cde0
stevanmatovic <stevan.matovic95@gmail.com>
2017-07-02 20:05:08 +0200

    message service metode dodate

--- src/ms/forum/services/MessageService.java
+++ src/ms/forum/services/MessageService.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -39,5 +40,55 @@ public class MessageService {
 		
 		return Response.status(200).build();
 	}
+	
+	
+	
+	
+	@POST
+	@Path("/get")
+	@Consumes({MediaType.TEXT_PLAIN})
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response get(@CookieParam("stevan") String value, int messageId) {	
+		
+		Response response = null;
+		UserDao userDAO = new UserDao();
+		
+		CookieDao cookieDao = new CookieDao();
+		int id = cookieDao.getUserIdByCookie(value);
+		
+		MessageDao messageDao = new MessageDao();
+		Message m = messageDao.getById(messageId);
+		
+		if(id ==  0 || id == -1 || m.getPrima() != id){
+			response = Response.status(401).build();
+			return response;
+		}
+		
+		
+		return Response.status(200).entity(m).build();
+	}
+	
+	
+	@POST
+	@Path("/getMessages")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response getMessages(@CookieParam("stevan") String value) {	
+		
+		Response response = null;
+		UserDao userDAO = new UserDao();
+		
+		CookieDao cookieDao = new CookieDao();
+		int id = cookieDao.getUserIdByCookie(value);
+		
+		if(id ==  0 || id == -1){
+			response = Response.status(401).build();
+			return response;
+		}
+		MessageDao messageDao = new MessageDao();
+		
+		return Response.status(200).entity(messageDao.getByUserId(id)).build();
+	}
+	
+	
 	
 }
